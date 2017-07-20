cb8bf75cbb7dcc38e18a7dc4b4ee8cdabaec72eb
yurividal <yurividal@gmail.com>
2017-07-20 09:07:29 +0100

    improve installer

--- install.sh
+++ install.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/sudo bash
 
-echo "Owning tcpdump"
-chown -R root /usr/sbin/tcpdump 
-chmod 4555 /usr/sbin/tcpdump 
-chmod +x tuxcdp
 
-echo "Installing tuxcdp"
-cp tuxcdp /usr/bin/
 
-echo "Install Complete"
+echo "Detecting tcpdump"
+if which tcpdump >/dev/null; then
+	echo "tcpdump detected, continuing"
+	echo "Owning tcpdump"
+	chown -R root /usr/sbin/tcpdump 
+	chmod 4555 /usr/sbin/tcpdump 
+	chmod +x tuxcdp
+
+	echo "Installing tuxcdp"
+	cp tuxcdp /usr/bin/
+
+	echo "Install Complete"
+
+else 
+	echo "tcpdump not found! "
+	echo "Please install tcpdump and run the installer again"
+fi
+
